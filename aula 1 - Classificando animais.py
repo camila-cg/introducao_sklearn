c4772406407fac5b88a63356d8406a3a9007db46
Camila Gusmão <camila.gusmao@radixeng.com.br>
2019-04-21 18:12:07 -0300

    Padronizando nomenclatura

--- aula 1 - Classificando animais.py
+++ aula 1 - Classificando animais.py
@@ -15,26 +15,25 @@ cachorro1 = [0, 1, 1]
 cachorro2 = [1, 0, 1]
 cachorro3 = [1, 1, 1]
 
+#modelo(dados) = classe --> f(x) = y
+
 # 1 => porco, 0 => cachorro
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-classes = [1,1,1,0,0,0]
+treino_x = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3] #features
+treino_y = [1,1,1,0,0,0]  #labels
 
 #Criando modelo
 model = LinearSVC()
-model.fit(dados, classes)
+model.fit(treino_x, treino_y)
 
 #Testando o modelo
-animal_misterioso = [1,1,1]
-model.predict([animal_misterioso])
-
 misterio1 = [1,1,1]
 misterio2 = [1,1,0]
 misterio3 = [0,1,1]  #porco q faz auau -> caso estranho
 
-testes = [misterio1, misterio2, misterio3]
-previsoes = model.predict(testes) #resultado do modelo
-testes_classes = [0, 1, 1] #resultado esperado
+teste_x = [misterio1, misterio2, misterio3] #features
+teste_y = [0, 1, 1] #labels
+previsoes = model.predict(teste_x) #resultado do modelo
 
 #Exibindo taxa de acerto
-taxa_de_acerto = accuracy_score(testes_classes, previsoes)
-print("Taxa de acerto", taxa_de_acerto * 100)
+taxa_de_acerto = accuracy_score(teste_y, previsoes)
+print("Taxa de acerto %.2f" %(taxa_de_acerto * 100))
